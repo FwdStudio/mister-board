df17e67376547c4cedcc062f814658cb2d959ee0
FwdStudio <gastronarrativa@gmail.com>
2026-08-10 15:54:12 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,31 @@
-const CACHE_NAME = 'squadra-manager-v3';
+const CACHE_NAME = 'squadra-manager-v2';
 const ASSETS = [
-  './',
   './index.html',
   './manifest.json',
   './icon-192.png',
   './icon-512.png'
 ];
 
-self.addEventListener('install', e => {
+self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+      .catch((err) => console.warn('Errore caricamento asset in cache:', err))
   );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
+self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
     )
   );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
+self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
+    caches.match(e.request).then((cached) => cached || fetch(e.request).catch(() => cached))
   );
 });
